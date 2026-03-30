27905bbe54db7e78000ff46afb80f422b08c0f6f
Jannik Menzel <jnk.menzel@gmail.com>
2026-03-30 17:38:59 +0200

    add TypeScript ESLint support; update ESLint config for Astro and TypeScript; fix minor HTML and structured data issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
-import js from "@eslint/js";
 import globals from "globals";
 import astro from "eslint-plugin-astro";
 import prettier from "eslint-plugin-prettier";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import astroParser from "astro-eslint-parser";
 
 export default [
     {
@@ -11,17 +13,63 @@ export default [
     {
         plugins: {
             prettier,
+            "@typescript-eslint": tseslint,
         },
     },
 
-    js.configs.recommended,
-    ...astro.configs["flat/recommended"],
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: globals.browser,
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+        },
+    },
+    {
+        files: ["**/*.astro"],
+        plugins: { astro },
+        rules: {
+            "astro/no-unused-css-selector": "warn",
+            "astro/no-set-html-directive": "off",
+        },
+    },
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        plugins: { "@typescript-eslint": tseslint },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
 
     {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts,astro}"],
-        ignores: ["**/*.d.ts"],
+        files: ["**/*.ts", "**/*.tsx"],
         languageOptions: {
-            globals: globals.browser,
+            parser: tsParser,
+            parserOptions: {
+                project: "./tsconfig.json",
+                sourceType: "module",
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+        },
+    },
+    {
+        files: ["**/*.astro"],
+        languageOptions: {
+            parser: astroParser,
+            parserOptions: {
+                parser: tsParser,
+                extraFileExtensions: [".astro"],
+            },
+        },
+        plugins: {
+            astro,
+            "@typescript-eslint": tseslint,
         },
     },
 ];
